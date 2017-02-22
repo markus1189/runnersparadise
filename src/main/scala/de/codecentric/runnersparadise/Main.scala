2959e766d9a85e67e890b6f5c4421f3951c911dc
Markus Hauck <markus1189@gmail.com>
2017-02-22 20:17:46 +0100

    Main that uses cassandra

--- src/main/scala/de/codecentric/runnersparadise/Main.scala
+++ src/main/scala/de/codecentric/runnersparadise/Main.scala
@@ -3,13 +3,24 @@ package runnersparadise
 
 import de.codecentric.runnersparadise.api.RaceRegistrationService
 import de.codecentric.runnersparadise.interpreters.InMemory
+import de.codecentric.runnersparadise.interpreters.cassandra.CassandraInterpreter
 import org.http4s.server.blaze.BlazeBuilder
 import org.http4s.server.{Server, ServerApp}
 
 import scalaz.concurrent.Task
 
-object Main extends ServerApp {
+object MainInMemory extends ServerApp {
   val interpreter = new InMemory
+  import interpreter._
+
+  val srv: RaceRegistrationService = new RaceRegistrationService
+
+  override def server(args: List[String]): Task[Server] =
+    BlazeBuilder.bindHttp(port = 8080, host = "localhost").mountService(srv.service, "/").start
+}
+
+object MainCassandra extends ServerApp {
+  val interpreter = new CassandraInterpreter
   import interpreter._
 
   val srv: RaceRegistrationService = new RaceRegistrationService
